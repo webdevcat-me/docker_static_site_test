c3bba5159ce27757131347f0e18d5623b7289aa5
Catalin Mititiuc <webdevcat@proton.me>
2024-04-13 12:48:51 -0700

    Initial commit

---
+++ lib/static_site.ex
@@ -0,0 +1,18 @@
+defmodule StaticSite do
+  @moduledoc """
+  Documentation for `StaticSite`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StaticSite.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
